2cdebcc9867a0283f513156bd05d609eec6d80d2
Key & Codes <andrewgalvez@outlook.com>
2023-01-17 15:29:10 -0500

    cpp_project9_UFOgame first commit

---
+++ ufo.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "ufo_functions.hpp"
+
+int main() {
+ 
+  std::string codeword = "codecademy";
+  std::string answer = "__________";
+  int misses = 0;
+  std::vector<char> incorrect;
+  bool guess = false;
+  char letter;
+
+  greet();
+ 
+    while (answer != codeword && misses < 7) {
+    display_misses(misses);
+    display_status(incorrect, answer);
+    std::cout << "\nPlease enter your guess: ";
+    std::cin >> letter;
+    for (int z=0; z < codeword.length(); z++) {
+      if (letter == codeword[z]) {
+        answer[z] = letter;
+        guess = true;
+      }
+    }  
+    if (guess) { 
+        std::cout << "Correct!";
+        }
+    else {
+        std::cout << "Incorrect! The tractor beam pulls the person in further.";
+        incorrect.push_back(letter);
+        misses++;
+      }   
+      guess = false;
+    }
+
+
+  end_game(answer,codeword);
+} 
